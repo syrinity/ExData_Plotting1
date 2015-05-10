41a507a6327168136c47a2c3fdb3201c68835be2
Meredith <meredith@syrinity.com>
2015-05-10 12:41:26 -0700

    final project
    Finished project

--- plot2.R
+++ plot2.R
@@ -1,27 +1,47 @@
+# This script creates a line graph for two days worth of data (02/01/07-02/02/07) from the UC Irvine 
+# Learning Repository.  More information on the data set and the assignment can be found in the 
+# README.md file on the github site https://github.com/syrinity/ExData_Plotting1.
+
+
+# Neccessary libraries needed to run the script
 library(lubridate)
 library(dplyr)
 
+# creates a directory off the current directory to download the data set to and to save the
+# plot to if it doesn't already exist.
 if(!file.exists("./powerConsumption")){dir.create("./powerConsumption")}
 
+# downloads and unzips the file containing the data
 fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
-
 download.file(fileUrl,destfile="./powerConsumption/powerConsumption.zip")
-
 unzip("./powerConsumption/powerConsumption.zip")
 
+# reads the text file into a data frame it explicitly states that NA is represented as a ? so that 
+# the values are correctly read in as numeric
 powerConsumption <- read.table("./powerConsumption/household_power_consumption.txt", header = TRUE, sep = ";",
                                na.strings="?")
 
+# converts the Date column into a Date data type
 powerConsumption$Date <- dmy(powerConsumption$Date)
 
-PowerDataSet <- filter(powerConsumption, Date > "2007-01-31" & Date < "2007-02-02")
+# subsets the data so that it only has the data for the sepcified 2 days
+PowerDataSet <- filter(powerConsumption, Date > "2007-01-31" & Date <= "2007-02-02")
 
+# converts the Time column using luberdate
 PowerDataSet$Time <- hms(PowerDataSet$Time)
 
-PowerDataSet$DateTime <- PowerDataSet$Date + PowerDataSet$Time
+# combines the Date and Time data into one column as a POSIXct data type
+PowerDataSet$datetime <- PowerDataSet$Date + PowerDataSet$Time
 
+# opens up a png file to save the plot to
 png("./powerConsumption/plot2.png")
-with(PowerDataSet, plot(DateTime, Global_active_power, type="n", 
+
+# creates an empty plot with the axes labeled
+with(PowerDataSet, plot(datetime, Global_active_power, type="n", 
      ylab="Global Active Power(kilowatts)", xlab="" ))
-with(PowerDataSet, lines(DateTime, Global_active_power))
+
+# adds the line
+with(PowerDataSet, lines(datetime, Global_active_power))
+
+# closes the file
 dev.off()
--- plot4.R
+++ plot4.R
@@ -1,36 +1,55 @@
+# This script creates a histogram for two days worth of data (02/01/07-02/02/07) from the UC Irvine 
+# Learning Repository.  More information on the data set and the assignment can be found in the 
+# README.md file on the github site https://github.com/syrinity/ExData_Plotting1.
+
+
+# Neccessary libraries needed to run the script
 library(lubridate)
 library(dplyr)
 
+# creates a directory off the current directory to download the data set to and to save the
+# plot to if it doesn't already exist.
 if(!file.exists("./powerConsumption")){dir.create("./powerConsumption")}
 
+# downloads and unzips the file containing the data
 fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
-
 download.file(fileUrl,destfile="./powerConsumption/powerConsumption.zip")
-
 unzip("./powerConsumption/powerConsumption.zip")
 
+# reads the text file into a data frame it explicitly states that NA is represented as a ? so that 
+# the values are correctly read in as numeric
 powerConsumption <- read.table("./powerConsumption/household_power_consumption.txt", header = TRUE, sep = ";",
                                na.strings="?")
 
+# converts the Date column into a Date data type
 powerConsumption$Date <- dmy(powerConsumption$Date)
 
-PowerDataSet <- filter(powerConsumption, Date > "2007-01-31" & Date < "2007-02-02")
+# subsets the data so that it only has the data for the sepcified 2 days
+PowerDataSet <- filter(powerConsumption, Date > "2007-01-31" & Date <= "2007-02-02")
 
+# converts the Time column using luberdate
 PowerDataSet$Time <- hms(PowerDataSet$Time)
 
+# combines the Date and Time data into one column as a POSIXct data type
 PowerDataSet$datetime <- PowerDataSet$Date + PowerDataSet$Time
 
-#png("./powerConsumption/plot4.png")
+# opens up a png file to save the plot to
+png("./powerConsumption/plot4.png")
 
+# sets up 4 plots (2 by 2)
 par(mfrow = c(2,2))
+
 with(PowerDataSet, {
+        # The first of 4 plots 
         plot(datetime, Global_active_power, type="n", 
              ylab="Global Active Power(kilowatts)", xlab="" )
         lines(datetime, Global_active_power)
-        
+
+        # The second of 4 plots         
         plot(datetime, Voltage, type="n")
         lines(datetime, Voltage)
 
+        # The third of 4 plots         
         plot(datetime, Sub_metering_1, type="n", 
              ylab="Energy sub metering", xlab="" )
         lines(datetime, Sub_metering_1)
@@ -39,8 +58,11 @@ with(PowerDataSet, {
         legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
                lty=c(1,1,1), col = c("black", "red", "blue"),bty = "n")
         
+        # The fourth of 4 plots         
         plot(datetime, Global_reactive_power, type="n")
         lines(datetime, Global_reactive_power)
 })  
-        
-#dev.off()
+
+# closes the file
+dev.off()
+
